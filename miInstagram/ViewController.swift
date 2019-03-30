701f38fe856e8bce56ead9fdd033f8b919f17dbe
Patricia <patricia@MacBook-20.local>
2019-03-30 14:05:38 -0600

    first commit

---
+++ miInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miInstagram
+//
+//  Created by patricia  D3 on 3/30/19.
+//  Copyright © 2019 patricia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ miInstagramTests/miInstagramTests.swift
@@ -0,0 +1,34 @@
+//
+//  miInstagramTests.swift
+//  miInstagramTests
+//
+//  Created by patricia  D3 on 3/30/19.
+//  Copyright © 2019 patricia. All rights reserved.
+//
+
+import XCTest
+@testable import miInstagram
+
+class miInstagramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
